f7f5dc450e533e1414747aaec1f289ef2ebbe260
mrTibet <vader.ctylxy@gmail.com>
2020-11-19 19:07:43 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-let next = document.getElementById('next');
+let nextButton = document.getElementById('next');
 let mainDiv = document.getElementById('container');
 let userRoll = document.getElementById('user-roll');
 
-next.addEventListener('click', function(){
-    next.parentElement.classList.add('passive');
+nextButton.addEventListener('click', function(){
+    nextButton.parentElement.classList.add('passive');
     container.children[1].classList.add('active');
 })
 
