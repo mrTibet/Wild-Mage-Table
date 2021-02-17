a91a0be5c449afc4bed8013f2fa1fda11eb298ff
Oleksandr Panforov <panforov.alex@gmail.com>
2021-02-17 23:12:05 +0200

    Added contents for the table

--- main.js
+++ main.js
@@ -3,12 +3,15 @@ let nextButton = document.getElementById('button-next'),
     userRoll = document.getElementById('user-roll'),
     userResult = document.getElementById('user-result'),
     appRoll = document.getElementById('app-roll'),
+    rollInput = document.getElementById('roll-input');
     backBtn = document.getElementById('result-back');
+    sliderDiv = document.querySelector('.slider');
+
 
 let allDiv = document.querySelectorAll('.block'),
     inputDiv = document.getElementsByTagName('form');
 
-nextButton.addEventListener('click', function(){
+nextButton.addEventListener('click', function () {
     nextButton.parentElement.classList.remove('active');
     nextButton.parentElement.classList.add('passive');
     container.children[1].classList.add('active');
@@ -24,12 +27,14 @@ userResult.addEventListener('click', function () {
     inputDiv[0].parentElement.classList.remove('active');
     inputDiv[0].parentElement.classList.add('passive');
     container.children[3].classList.add('active');
+    getDataById(rollInput.valueAsNumber);
 })
 
 appRoll.addEventListener('click', function () {
     appRoll.parentElement.classList.remove('active');
     appRoll.parentElement.classList.add('passive');
     container.children[3].classList.add('active');
+    getDataById(Math.floor(Math.random() * Math.floor(100)));
 })
 
 backBtn.addEventListener('click', function () {
@@ -38,6 +43,23 @@ backBtn.addEventListener('click', function () {
     container.children[0].classList.add('active');
 })
 
+const rollData = JSON.parse(localStorage.getItem('rollData'));
 
+function getDataById(id) {
+    const descriptions = rollData[id-1].descriptions;
 
+    sliderDiv.innerHTML = descriptions.map(d => `<section><div>Table ${d.tableId}</div><br> <div>${d.description}</div></section>`).join('');
+}
 
+function handleChange(input) {
+    if (input.value < 0) input.value = 0;
+    if (input.value > 100) input.value = 100;
+}
+
+function handleRollChange(value) {
+    if (value <= 0 || value > 100) {
+        userResult.disabled = true;
+    } else {
+        userResult.disabled = false;
+    }
+}
